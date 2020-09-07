cc2591127bbe7d2e316fab84f6a9ccec75e4f6aa
Grigori <jlavyan@gmail.com>
2020-09-07 17:51:30 +0400

    ADD create with Json

--- lib/realm.dart
+++ lib/realm.dart
@@ -19,6 +19,7 @@ class Realm {
 
   static const MethodChannel _channel =
       const MethodChannel('flutterrealm_light');
+
   SyncUser _syncUser;
   String _appId;
   String _partition;
@@ -35,8 +36,16 @@ class Realm {
   /// [param] _creator required for make object for given generic type
   Future<T> create<T extends RLMObject>(ItemCreator _creator, T value,
       {UpdatePolicy policy = UpdatePolicy.error}) async {
+    return createWithJson(_creator, value.toJson(), policy: policy);
+  }
+
+  /// Create object by given policy.
+  ///
+  /// [param] _creator required for make object for given generic type
+  Future<T> createWithJson<T extends RLMObject>(ItemCreator _creator, Map value,
+      {UpdatePolicy policy = UpdatePolicy.error}) async {
     Map<String, dynamic> values = {
-      'value': value.toJson(),
+      'value': value,
       'policy': policy.value,
       'identity': _syncUser.identity,
       'appId': _appId,
